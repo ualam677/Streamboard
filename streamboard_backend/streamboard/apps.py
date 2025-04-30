f6023b572e4c6be43436b8c541d2438a6cbd34a4
Kay <55361516+ualam677@users.noreply.github.com>
2025-04-30 16:43:23 +0100

    sidebar, preview, more customisation

--- streamboard_backend/streamboard/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class StreamboardConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "streamboard"
--- streamboard_backend/streamboard/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import StreamboardCreateView, StreamboardDetailView
-
-urlpatterns = [
-    path('', StreamboardCreateView.as_view(), name='streamboard-create'),
-    path('<uuid:id>/', StreamboardDetailView.as_view(), name='streamboard-detail'),
-
-]
